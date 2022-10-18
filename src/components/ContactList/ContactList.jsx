86fed40d1ad0b08c18cfe83d28de2ca646daa29b
Kateryna Berehovych <kasia-94@ukr.net>
2022-10-18 22:08:59 +0300

    Add prev homework

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ContactItem } from 'components/ContactItem/ContactItem';
+import { List } from './ContactList.styled';
+
+export const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <List>
+      {contacts.map((contact, id) => (
+        <ContactItem
+          key={id}
+          contact={contact}
+          onDeleteContact={onDeleteContact}
+        />
+      ))}
+    </List>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      contact: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
